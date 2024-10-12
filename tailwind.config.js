8945974d57ece127e46e362712fed46e97f35b7f
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-12 17:30:50 +0300

    add: shadcn CSS variables for colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,21 @@ export default {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-  		colors: {}
+  		colors: {
+        'border': 'var(--border)',
+        'input': 'var(--input)',
+        'ring': 'var(--ring)',
+        'background': 'var(--background)',
+        'foreground': 'var(--foreground)',
+        'primary': 'var(--primary)',
+        'primary-foreground': 'var(--primary-foreground)',
+        'secondary': 'var(--secondary)',
+        'secondary-foreground': 'var(--secondary-foreground)',
+        'accent': 'var(--accent)',
+        'accent-foreground': 'var(--accent-foreground)',
+        'destructive': 'var(--destructive)',
+        'destructive-foreground': 'var(--destructive-foreground)',
+      }
   	}
   },
   plugins: [require("tailwindcss-animate")],
