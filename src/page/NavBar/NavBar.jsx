e530dcdcaf2f25c75d4091a10569e5ca8e80f5ab
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-15 17:56:25 +0300

    add: add search bar and user avatar in navbar component

--- src/page/NavBar/NavBar.jsx
+++ src/page/NavBar/NavBar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
-import { DragHandleHorizontalIcon } from "@radix-ui/react-icons";
-import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { DragHandleHorizontalIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import Sidebar from "./Sidebar";
 
 
@@ -10,38 +10,52 @@ const NavBar = () => {
     return(
         <div className="px-2 py-3 border-b z-50 bg-background bg-opacity-0 sticky top-0 left-0 right-0 flex justify-between items-center">
             <div className="flex items-center gap-3">
-            <Sheet>
-  <SheetTrigger>
-    <Button 
-    variant="ghost" 
-    size="icon" 
-    className="rounded-full h-11 w-11"
-    >
-        <DragHandleHorizontalIcon className="h-7 w-7"/>
-    </Button>
-  </SheetTrigger>
-  <SheetContent 
-  className="w-72 border-r-0 flex flex-col justify center" 
-  side="left">
-    <SheetHeader>
-      <SheetTitle>
-        <div className="text-3x1 flex justify-center items-center gap-1">
-            <Avatar>
-                <AvatarImage src="https://i.ebayimg.com/images/g/WroAAOSwKXti3ugj/s-l1600.webp"></AvatarImage>
-            </Avatar>
+              <Sheet>
+    <SheetTrigger>
+      <Button  
+      size="icon" 
+      className="rounded-full h-11 w-11"
+      >
+          <DragHandleHorizontalIcon className="h-7 w-7"/>
+      </Button>
+    </SheetTrigger>
+    <SheetContent 
+    className="w-72 border-r-0 flex flex-col justify center" 
+    side="left">
+      <SheetHeader>
+        <SheetTitle>
+          <div className="text-3x1 flex justify-center items-center gap-1">
+              <Avatar>
+                  <AvatarImage src="https://i.ebayimg.com/images/g/WroAAOSwKXti3ugj/s-l1600.webp"></AvatarImage>
+              </Avatar>
+              <div>
+                  <span className="font-bold">Trade404</span>
+              </div>
+          </div>
+        </SheetTitle>
+      </SheetHeader>
+      <Sidebar/>
+    </SheetContent>
+              </Sheet>
+              <p className="text-sm lg:text-base cursor-pointer">
+                Trade404
+              </p>
+              <div className="p-0 ml-9">
+                  <Button variant="outline"
+                  className="flex items-center gap-3">
+                      <MagnifyingGlassIcon/>
+                      <span>Search</span>
+                  </Button>
+              </div>
+            </div>
             <div>
-                <span className="font-bold">Trade404</span>
+              <Avatar>
+                  <AvatarFallback>
+                      D
+                  </AvatarFallback>
+              </Avatar>
             </div>
         </div>
-      </SheetTitle>
-    </SheetHeader>
-    <Sidebar/>
-  </SheetContent>
-</Sheet>
-
-            </div>
-
-            </div>
     )
 }
 
