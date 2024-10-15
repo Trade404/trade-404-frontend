2fbff8bd7d4e966b11df56f68c60d16422fb354d
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-15 17:57:22 +0300

    add: create initial Home component for future implementation and add it to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Home from './page/Home/Home';
 import NavBar from './page/NavBar/NavBar';
 
 function App() {
   return (
     <>
-      <NavBar />
+      <NavBar/>
+      <Home/>
     </>
   );
 }
