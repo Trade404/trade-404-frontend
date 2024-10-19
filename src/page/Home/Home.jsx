9b13a3e30a2fd87517811b7e1e1164ba78e448da
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-19 18:22:28 +0300

    add: chat bot button and prompt with style

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -4,6 +4,7 @@ import AssetTable from "./AssetTable";
 import StockChart from "./StockChart";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { DotIcon, MessageCircle } from "lucide-react";
+import { Cross1Icon } from "@radix-ui/react-icons";
 
 const Home = () => {
 
@@ -70,6 +71,20 @@ const Home = () => {
                 </div>
             </div>
             <section className="absolute bottom-5 right-5 z-40 flex flex-col justify-end items-end gap-2">
+                <div className="rounded-md w-[20rem] md:w-[25rem] lg:w-[25rem] h-[70vh] bg-slate-900">
+                    <div className="flex justify-between items-center border-b px-6 h-[12%]">                    
+                        <p>Chat Bot</p>
+                        <Button variant="ghost" size="icon">
+                            <Cross1Icon/>
+                        </Button>
+                        </div>
+                        <div className="h-[76%] flex flex-col overflow-y-auto gap-5 px-5 py-2 scroll-container">
+                            <div className="justify-end self-end px-5 py-2 rounded-md bg-slate-800 w-auto">
+                                <p>Hi</p>
+                                <p>you can ask crypto related questions</p>
+                            </div>
+                        </div>
+                    </div>
                 <div className="relative w-[10rem] cursor-pointer group">
                     <Button className="w-full h-[3rem] gap-2 items-center">
                         <MessageCircle size={30} 
