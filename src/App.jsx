474f9dd361a268b537de33dabab9bc62443c513d
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-21 16:12:37 +0300

    add: Watchlist base component implementation for routing usage and added routes for all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { Route, Routes } from 'react-router-dom';
 import Home from './page/Home/Home';
 import NavBar from './page/NavBar/NavBar';
+import Portfolio from './page/Portfolio/Portfolio';
+import Wallet from './page/Wallet/Wallet';
+import Activity from './page/Activity/Activity';
+import Withdrawal from './page/Withdrawal/Withdrawal';
+import PaymentDetails from './page/PaymentDetails/PaymentDetails';
+import StockDetails from './page/StockDetails/StockDetails';
+import Profile from './page/Profile/Profile';
+import Watchlist from './page/Watchlist/Watchlist';
+import SearchCoin from './page/Search/SearchCoin';
+import Notfound from './page/Notfound/Notfound';
+
 
 function App() {
   return (
     <>
       <NavBar/>
-      <Home/>
+      <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/portfolio" element={<Portfolio/>} />
+          <Route path="/activity" element={<Activity/>} />
+          <Route path="/wallet" element={<Wallet/>} />
+          <Route path="/withdrawal" element={<Withdrawal/>} />
+          <Route path="/payment-details" element={<PaymentDetails/>} />
+          <Route path="/market/:id" element={<StockDetails/>} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/watchlist" element={<Watchlist/>} />
+          <Route path="/search" element={<SearchCoin/>} />
+          <Route path="*" element={<Notfound/>} />
+      </Routes>
     </>
   );
 }
---
+++ src/page/Watchlist/Watchlist.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Watchlist = () => {
+    return ()
+}
+
+export default Watchlist
