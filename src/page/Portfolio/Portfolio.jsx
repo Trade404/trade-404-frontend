2af8d5a972e8657052d5c57ce5f136b87383db64
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-22 16:11:34 +0300

    add: Watchlist component table customized with specific fields and remove frow watchlist button

--- src/page/Portfolio/Portfolio.jsx
+++ src/page/Portfolio/Portfolio.jsx
@@ -8,12 +8,11 @@ const Portfolio = () => {
                 <Table>
                     <TableHeader>
                         <TableRow>
-                        <TableHead className="w-[100px]">Coin</TableHead>
+                        <TableHead className="w-[100px]">Asset</TableHead>
                         <TableHead>Price</TableHead>
                         <TableHead>Unit</TableHead>
                         <TableHead>Change</TableHead>
                         <TableHead>Change(%)</TableHead>
-                        <TableHead>Value</TableHead>
                         <TableHead className="text-right">Value</TableHead>
                         </TableRow>
                     </TableHeader>
@@ -28,23 +27,10 @@ const Portfolio = () => {
                         </TableCell>
                         <TableCell>BTC</TableCell>
                         <TableCell>9126357982</TableCell>
+                        <TableCell>-503.41</TableCell>
                         <TableCell>-0.20009</TableCell>
-                        <TableCell>69544</TableCell>
                         <TableCell className="text-right">$69249</TableCell>
                         </TableRow>)}
-                        <TableRow>
-                        <TableCell className="font-medium flex items-center gap-2">
-                            <Avatar className="-z-50">
-                                <AvatarImage src="https://assets.coingecko.com/coins/images/1/standard/bitcoin.png?1696501400"/>
-                            </Avatar>
-                            <span>Bitcoin</span>
-                        </TableCell>
-                        <TableCell>BTC</TableCell>
-                        <TableCell>9126357982</TableCell>
-                        <TableCell>-0.20009</TableCell>
-                        <TableCell>69544</TableCell>
-                        <TableCell className="text-right">$69249</TableCell>
-                        </TableRow>
                     </TableBody>
                 </Table>
     </div>)
--- src/page/Watchlist/Watchlist.jsx
+++ src/page/Watchlist/Watchlist.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { Button } from "@/components/ui/button";
+import { BookmarkFilledIcon } from "@radix-ui/react-icons";
 
 const Watchlist = () => {
-    return (<div></div>)
+
+    const handleRemoveToWatchlist = (value) => {
+        console.log(value)
+    }
+    return (
+        <div className="p-5 lg:p-20">
+        <h1 className="font-bold text-3xl pb-5">Watchlist</h1>
+        <Table className="border">
+  <TableHeader>
+    <TableRow>
+      <TableHead className="py-5">Coin</TableHead>
+      <TableHead>Symbol</TableHead>
+      <TableHead>Volume</TableHead>
+      <TableHead>Market Cap</TableHead>
+      <TableHead>24h</TableHead>
+      <TableHead>Price</TableHead>
+      <TableHead className="text-right">Remove</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    {[1,1,1,1,1,1].map((item, index) =>     
+    <TableRow key={index}>
+      <TableCell className="font-medium flex items-center gap-2">
+        <Avatar className="-z-50">
+            <AvatarImage src="https://assets.coingecko.com/coins/images/1/standard/bitcoin.png?1696501400"/>
+        </Avatar>
+        <span>Bitcoin</span>
+      </TableCell>
+      <TableCell>BTC</TableCell>
+      <TableCell>9126357982</TableCell>
+      <TableCell>69544</TableCell>
+      <TableCell>-0.20009</TableCell>
+      <TableCell>$69249</TableCell>
+      <TableCell className="text-right">
+        <Button variant="ghost" onClick={() => handleRemoveToWatchlist(item.id)} size="icon" className="h-10 w-10">
+            <BookmarkFilledIcon className="w-6 h-6" />
+        </Button>
+      </TableCell>
+    </TableRow>)}
+    <TableRow>
+      <TableCell className="font-medium flex items-center gap-2">
+        <Avatar className="-z-50">
+            <AvatarImage src="https://assets.coingecko.com/coins/images/1/standard/bitcoin.png?1696501400"/>
+        </Avatar>
+        <span>Bitcoin</span>
+      </TableCell>
+      <TableCell>BTC</TableCell>
+      <TableCell>9126357982</TableCell>
+      <TableCell>-0.20009</TableCell>
+      <TableCell>69544</TableCell>
+      <TableCell className="text-right">$69249</TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
+</div>)
 }
 
 export default Watchlist
