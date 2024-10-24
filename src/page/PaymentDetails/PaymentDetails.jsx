18b036234ecaa926409bc30ec510229e5e7893dc
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-24 16:03:42 +0300

    add: PaymentDetails component functionality and style using test data

--- src/page/PaymentDetails/PaymentDetails.jsx
+++ src/page/PaymentDetails/PaymentDetails.jsx
@@ -1,7 +1,48 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button";
+import PaymentDetailsForm from "./PaymentDetailsForm";
+  
 
 const PaymentDetails = () => {
-    return (<div></div>)
+    return (
+    <div className="px-20">
+        <h1 className="text-3xl font-bold py-10">Payment Details</h1>
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    IF Bank
+                </CardTitle>
+                <CardDescription>
+                    ************5271
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="flex items-center">
+                    <p className="w-32">Account Holder</p>
+                    <p className="text-gray-400">: DS</p>
+                </div>
+                <div className="flex items-center">
+                    <p className="w-32">IFSC</p>
+                    <p className="text-gray-400">: IFB0000008</p>
+                </div>
+            </CardContent>
+        </Card>
+        <Dialog>
+            <DialogTrigger>
+                <Button className="py-6">Add Payment Details</Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Payment Details</DialogTitle>
+                </DialogHeader>
+                <PaymentDetailsForm/>
+            </DialogContent>
+        </Dialog>
+
+    </div>
+)
 }
 
 export default PaymentDetails
