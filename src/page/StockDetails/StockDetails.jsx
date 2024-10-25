d1ae3c777feb618ffc41ff3587185c94ae0607ff
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-25 16:23:30 +0300

    add: initial implementation of StockDetails component and navigation from AssetTable cells

--- src/page/StockDetails/StockDetails.jsx
+++ src/page/StockDetails/StockDetails.jsx
@@ -1,7 +1,32 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { DotIcon } from "@radix-ui/react-icons";
 import React from "react";
 
 const StockDetails = () => {
-    return (<div></div>)
+    return (
+    <div className="p-5 mt-5">
+        <div className="flex justify-between">
+            <div className="flex gap-5 items-center">
+                <Avatar>
+                    <AvatarImage/>
+                </Avatar>
+            </div>
+            <div>
+                <div className="flex items-center gap-2">
+                    <p>BTC</p>
+                    <DotIcon className="text-gray-400"/>
+                    <p className="text-gray-400">Bitcoin</p>
+                </div>
+                <div className="flex items-end gap-2">
+                    <p className="text-xl font-bold">$7180</p>
+                    <p className="text-red-600">
+                        <span>-1525.332</span>
+                        <span>-(1.563%)</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>)
 }
 
 export default StockDetails
