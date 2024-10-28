8483882bfe295d2e349938af3e54e6aeb33cf422
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-28 17:26:26 +0200

    add: implement SignInForm component

--- src/page/Auth/SignInForm.jsx
+++ src/page/Auth/SignInForm.jsx
@@ -1,5 +1,63 @@
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { useForm } from "react-hook-form"
+
 const SignInForm = () => {
-    <div></div>
+    
+    const form = useForm({
+        resolver:"",
+        defaultValues:{
+            fullName:"",
+            email:"",
+            password:""
+        }
+    })
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+    return (
+        <div>
+            <h1 className="text-xl font-bold text-center pb-3">Login</h1>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                    <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem >
+                        <FormControl>
+                            <Input 
+                            className="border w-full border-gray-700 p-5"
+                            placeholder="ds@gmail.com" 
+                            {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem >
+                        <FormControl>
+                            <Input 
+                            className="border w-full border-gray-700 p-5"
+                            placeholder="your password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    <Button type="submit" className="w-full py-5">
+                        Submit
+                    </Button>               
+                </form>
+            </Form>
+        </div>
+    )
 }
 
 export default SignInForm
