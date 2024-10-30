951a527c6ca0953db8cc17decf740528b617b8b0
DilyanSvetiev <dsvetiev@gmail.com>
2024-10-30 15:38:19 +0200

    add: additional cases and actions in AuthReducer

--- state/auth/Reducer.js
+++ state/auth/Reducer.js
@@ -1,4 +1,4 @@
-const { REGISTER_REQUEST, REGISTER_SUCCESS, REGISTER_FAILURE } = require("./ActionTypes")
+import { REGISTER_REQUEST, REGISTER_SUCCESS, REGISTER_FAILURE, LOGIN_REQUEST, GET_USER_REQUEST, LOGIN_SUCCESS, GET_USER_SUCCESS, LOGIN_FAILURE } from "./ActionTypes"
 
 const initialState = {
     user:null,
@@ -10,10 +10,19 @@ const initialState = {
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
         case REGISTER_REQUEST:
+        case LOGIN_REQUEST:
+        case GET_USER_REQUEST:
             return({...state, loading:true, error:null})
+
         case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
             return {...state, loading:false, error:null, jwt:action.payload}
+
+        case GET_USER_SUCCESS:
+            return {...state, user:action.payload, loading:false, error:null}
+
         case REGISTER_FAILURE:
+        case LOGIN_FAILURE:
             return {...state, loading:false, error:action.payload}
         
         default:
