778fad4822b90a686cea77c2dc22f1c808e5287a
DilyanSvetiev <dsvetiev@gmail.com>
2024-11-01 14:44:18 +0200

    add: implement getTop50CoinsList function in Action.js

--- src/state/coin/Action.js
+++ src/state/coin/Action.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { FETCH_COIN_LIST_FAILURE, FETCH_COIN_LIST_REQUEST, FETCH_COIN_LIST_SUCCESS } from "./ActionTypes"
+import { FETCH_COIN_LIST_FAILURE, FETCH_COIN_LIST_REQUEST, FETCH_COIN_LIST_SUCCESS, FETCH_TOP_50_COINS_FAILURE, FETCH_TOP_50_COINS_REQUEST, FETCH_TOP_50_COINS_SUCCESS } from "./ActionTypes"
 
 export const getCoinList = (page) => async(dispatch) => {
 
@@ -15,6 +15,24 @@ export const getCoinList = (page) => async(dispatch) => {
 
     } catch(error) {
         dispatch({type:FETCH_COIN_LIST_FAILURE, payload:error.message})
+        console.log(error)
+    }
+    
+}
+
+export const getTop50CoinsList= (pae) => async(dispatch) => {
+
+    dispatch({type:FETCH_TOP_50_COINS_REQUEST})
+
+    const baseUrl = "http://localhost:4040"
+
+    try {
+        const response =  await axios.get(`${baseUrl}/coins/top50`)
+        dispatch({type:FETCH_TOP_50_COINS_SUCCESS, payload:response.data})
+        console.log(response.data)
+
+    } catch(error) {
+        dispatch({type:FETCH_TOP_50_COINS_FAILURE, payload:error.message})
         console.log(error)
     }
     
