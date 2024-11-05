873c77434f148981bf5bd2202e378a6a16e57908
DilyanSvetiev <dsvetiev@gmail.com>
2024-11-05 19:48:26 +0200

    add: implement fetchCoinDetails and searchCoin functions

--- src/state/coin/Action.js
+++ src/state/coin/Action.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { FETCH_COIN_BY_ID_FAILURE, FETCH_COIN_BY_ID_REQUEST, FETCH_COIN_BY_ID_SUCCESS, FETCH_COIN_LIST_FAILURE, FETCH_COIN_LIST_REQUEST, FETCH_COIN_LIST_SUCCESS, FETCH_MARKET_CHART_FAILURE, FETCH_MARKET_CHART_REQUEST, FETCH_MARKET_CHART_SUCCESS, FETCH_TOP_50_COINS_FAILURE, FETCH_TOP_50_COINS_REQUEST, FETCH_TOP_50_COINS_SUCCESS } from "./ActionTypes"
+import { FETCH_COIN_BY_ID_FAILURE, FETCH_COIN_BY_ID_REQUEST, FETCH_COIN_BY_ID_SUCCESS, FETCH_COIN_DETAILS_FAILURE, FETCH_COIN_DETAILS_REQUEST, FETCH_COIN_DETAILS_SUCCESS, FETCH_COIN_LIST_FAILURE, FETCH_COIN_LIST_REQUEST, FETCH_COIN_LIST_SUCCESS, FETCH_MARKET_CHART_FAILURE, FETCH_MARKET_CHART_REQUEST, FETCH_MARKET_CHART_SUCCESS, FETCH_TOP_50_COINS_FAILURE, FETCH_TOP_50_COINS_REQUEST, FETCH_TOP_50_COINS_SUCCESS, SEARCH_COIN_FAILURE, SEARCH_COIN_REQUEST, SEARCH_COIN_SUCCESS } from "./ActionTypes"
 import { api, API_BASE_URL } from "@/config/api"
 
 export const getCoinList = (page) => async(dispatch) => {
@@ -69,6 +69,36 @@ export const fetchCoinById= (coinId) => async(dispatch) => {
 
     } catch(error) {
         dispatch({type:FETCH_COIN_BY_ID_FAILURE, payload:error.message})
+        console.log(error)
+    }
+}
+
+export const fetchCoinDetails= (coinId) => async(dispatch) => {
+
+    dispatch({type:FETCH_COIN_DETAILS_REQUEST})
+
+    try {
+        const response =  await api.get(`${API_BASE_URL}/coins/${coinId}`)
+        dispatch({type:FETCH_COIN_DETAILS_SUCCESS, payload:response.data})
+        console.log(response.data)
+
+    } catch(error) {
+        dispatch({type:FETCH_COIN_DETAILS_FAILURE, payload:error.message})
+        console.log(error)
+    }
+}
+
+export const searchCoin= (keyword) => async(dispatch) => {
+
+    dispatch({type:SEARCH_COIN_REQUEST})
+
+    try {
+        const response =  await api.get(`${API_BASE_URL}/coins/${keyword}`)
+        dispatch({type:SEARCH_COIN_SUCCESS, payload:response.data})
+        console.log(response.data)
+
+    } catch(error) {
+        dispatch({type:SEARCH_COIN_FAILURE, payload:error.message})
         console.log(error)
     }
 }
