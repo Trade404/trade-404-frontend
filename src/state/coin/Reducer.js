6d00fd3585ce501407fcc0fd256527c8f4ee2995
DilyanSvetiev <dsvetiev@gmail.com>
2024-11-07 22:51:25 +0200

    add: initialState to Reducer.js

---
+++ src/state/coin/Reducer.js
@@ -0,0 +1,11 @@
+
+const initialState = {
+    coinList: [],
+    top50: [],
+    searchCoinList: [],
+    marketChart: {data:[], loading:[]},
+    coinById: null,
+    coinDetails: null,
+    loading: false,
+    error: null
+}
