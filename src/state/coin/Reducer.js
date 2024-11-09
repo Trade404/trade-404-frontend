5164a97e587c238bfa5685d0d95a2ec6e84840fe
DilyanSvetiev <dsvetiev@gmail.com>
2024-11-09 14:38:13 +0200

    add: finalized implementation of coinReducer

--- src/state/coin/Reducer.js
+++ src/state/coin/Reducer.js
@@ -1,6 +1,7 @@
-const { data } = require("autoprefixer");
-const { FETCH_COIN_LIST_REQUEST, FETCH_COIN_BY_ID_REQUEST, FETCH_COIN_DETAILS_REQUEST, SEARCH_COIN_REQUEST, FETCH_TOP_50_COINS_REQUEST, FETCH_MARKET_CHART_REQUEST, FETCH_COIN_LIST_SUCCESS, FETCH_TOP_50_COINS_SUCCESS, FETCH_MARKET_CHART_SUCCESS, SEARCH_COIN_SUCCESS } = require("./ActionTypes");
 const { error } = require("console");
+const { FETCH_COIN_LIST_REQUEST, FETCH_COIN_BY_ID_REQUEST, FETCH_COIN_DETAILS_REQUEST, SEARCH_COIN_REQUEST, FETCH_TOP_50_COINS_REQUEST, FETCH_MARKET_CHART_REQUEST, FETCH_COIN_LIST_SUCCESS, FETCH_TOP_50_COINS_SUCCESS, FETCH_MARKET_CHART_SUCCESS, SEARCH_COIN_SUCCESS, FETCH_COIN_DETAILS_SUCCESS, FETCH_MARKET_CHART_FAILURE, FETCH_COIN_LIST_FAILURE, SEARCH_COIN_FAILURE, FETCH_COIN_BY_ID_FAILURE, FETCH_COIN_DETAILS_FAILURE, FETCH_TOP_50_COINS_FAILURE } = require("./ActionTypes");
+const { ReceiptEuro } = require("lucide-react");
+
 
 const initialState = {
     coinList: [],
@@ -28,33 +29,59 @@ const coinReducer = (state = initialState, action) => {
                 marketChart: {loading: true, data: []},
                 error: null
             };
-            case FETCH_COIN_LIST_SUCCESS:
+        case FETCH_COIN_LIST_SUCCESS:
                 return {
                     ...state,
                     coinList: action.payload,
                     loading: false,
                     error: null
                 };
-                case FETCH_TOP_50_COINS_SUCCESS:
+        case FETCH_TOP_50_COINS_SUCCESS:
                     return {
                         ...state,
                         top50: action.payload,
                         loading: false,
                         error: null
                     };
-                case FETCH_MARKET_CHART_SUCCESS:
+        case FETCH_MARKET_CHART_SUCCESS:
                     return {
                         ...state,
                         marketChart: {data: action.payload.prices, loading: false},
                         error: null
                     };
-                    case SEARCH_COIN_SUCCESS:
+        case SEARCH_COIN_SUCCESS:
                         return {
                             ...state,
                             searchCoinList: action.payload.coins,
                             error: null,
                             loading: false
                         };
-        
+        case FETCH_COIN_DETAILS_SUCCESS:
+            return {
+                ...state,
+                coinDetails: action.payload,
+                loading: false,
+                error: null
+            }
+        case FETCH_MARKET_CHART_FAILURE:
+            return {
+                ...state,
+                marketChart: {loading: false, data: []},
+                error: null
+            };
+        case FETCH_COIN_LIST_FAILURE:
+        case SEARCH_COIN_FAILURE:
+        case FETCH_COIN_BY_ID_FAILURE:
+        case FETCH_COIN_DETAILS_FAILURE:
+        case FETCH_TOP_50_COINS_FAILURE:
+            return {
+                ...state,
+                loading:false,
+                error: action.payload
+            }
+        default:
+            return state;
     }
 }
+
+export default coinReducer;
