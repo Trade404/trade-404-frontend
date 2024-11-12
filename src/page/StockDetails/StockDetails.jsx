d4725a27a0fef20652f15a183558f674a0b489d3
DilyanSvetiev <dsvetiev@gmail.com>
2024-11-12 17:40:14 +0200

    add: fetched data usage instead of test data

--- src/page/StockDetails/StockDetails.jsx
+++ src/page/StockDetails/StockDetails.jsx
@@ -1,32 +1,43 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { BookmarkFilledIcon, BookmarkIcon, DotIcon } from "@radix-ui/react-icons";
-import React from "react";
 import TradingForm from "./TradingForm";
 import StockChart from "../Home/StockChart";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchCoinDetails } from "@/state/coin/Action";
+import { store } from "@/state/Store";
 
 const StockDetails = () => {
+
+    const {coin} = useSelector(store => store)
+        const dispatch = useDispatch()
+    const {id} = useParams()
+
+    useEffect(() => {
+        dispatch(fetchCoinDetails({coinId:id, jwt:localStorage.getItem("jwt")}))
+    },[id])
     return (
     <div className="p-5 mt-5">
         <div className="flex justify-between">
             <div className="flex gap-5 items-center">
                 <div>                
                     <Avatar>
-                        <AvatarImage/>
+                        <AvatarImage src={coin.coinDetails?.image.large}/>
                     </Avatar>
                 </div>
                 <div>
                     <div className="flex items-center gap-2">
                         <p>BTC</p>
                         <DotIcon className="text-gray-400"/>
-                        <p className="text-gray-400">Bitcoin</p>
+                        <p className="text-gray-400">{coin.coinDetails?.name}</p>
                     </div>
                     <div className="flex items-end gap-2">
-                        <p className="text-xl font-bold">$7180</p>
+                        <p className="text-xl font-bold">{coin.coinDetails?.market_data.current_price.usd}</p>
                         <p className="text-red-600">
-                            <span>-1525.332</span>
-                            <span>-(1.563%)</span>
+                            <span>{coin.coinDetails?.market_data.market_cap_change_24h}</span>
+                            <span>{coin.coinDetails?.market_data.market_cap_change_percentage_24h}%</span>
                         </p>
                     </div>
                 </div>
