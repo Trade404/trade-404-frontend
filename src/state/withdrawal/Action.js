edcf47d96f04dd39577d8760fd4edd5032b4fa68
DilyanSvetiev <dsvetiev@gmail.com>
2024-11-19 17:50:08 +0200

    add: implement functions for proceeding, geting history and getting all withdrawal requests

--- src/state/withdrawal/Action.js
+++ src/state/withdrawal/Action.js
@@ -1,11 +1,11 @@
 import { api } from "@/config/api"
-import { WITHDRAWAL_FAILURE, WITHDRAWAL_PROCEED_REQUEST, WITHDRAWAL_REQUEST, WITHDRAWAL_SUCCESS } from "./ActionTypes"
+import { GET_WITHDRAWAL_HISTORY_FAILURE, GET_WITHDRAWAL_HISTORY_REQUEST, GET_WITHDRAWAL_HISTORY_SUCCESS, GET_WITHDRAWAL_REQUEST_FAILURE, GET_WITHDRAWAL_REQUEST_REQUEST, GET_WITHDRAWAL_REQUEST_SUCCESS, WITHDRAWAL_FAILURE, WITHDRAWAL_PROCEED_FAILURE, WITHDRAWAL_PROCEED_REQUEST, WITHDRAWAL_PROCEED_SUCCESS, WITHDRAWAL_REQUEST, WITHDRAWAL_SUCCESS } from "./ActionTypes"
 
 export const withdrawalRequest = ({amount, jwt}) => async dispatch => {
-    dispatch({ type: WITHDRAWAL_REQUEST})
+    dispatch({ type: WITHDRAWAL_REQUEST })
 
     try {
-        const response = await api.get(`/api/withdrawal/${  amount}`, null, {
+        const response = await api.get(`/api/withdrawal/${amount}`, null, {
             headers: {
                 Authorization: `Bearer ${jwt}`
             }
@@ -23,4 +23,73 @@ export const withdrawalRequest = ({amount, jwt}) => async dispatch => {
         })
     }
     
+}
+
+export const proceedWithdrawal = ({id, jwt, accept}) => async dispatch => {
+    dispatch({ type: WITHDRAWAL_PROCEED_REQUEST })
+
+    try {
+        const response = await api.patch(`/api/admin/withdrawal/${id}/proceed/${accept}`, null, {
+            headers: {
+                Authorization: `Bearer ${jwt}`
+            }
+        })
+        
+        dispatch({
+            type: WITHDRAWAL_PROCEED_SUCCESS,
+            payload: response.data
+       })
+    } catch (error) {
+        console.log(error)
+        dispatch({
+            type: WITHDRAWAL_PROCEED_FAILURE,
+            payload: error.message
+        })
+    }
+}
+
+export const getWIthdrawalHistory = jwt => async dispatch => {
+    dispatch({ type: GET_WITHDRAWAL_HISTORY_REQUEST })
+
+    try {
+        const response = await api.get(`/api/withdrawal`, null, {
+            headers: {
+                Authorization: `Bearer ${jwt}`
+            }
+        })
+        
+        dispatch({
+            type: GET_WITHDRAWAL_HISTORY_SUCCESS,
+            payload: response.data
+       })
+    } catch (error) {
+        console.log(error)
+        dispatch({
+            type: GET_WITHDRAWAL_HISTORY_FAILURE,
+            payload: error.message
+        })
+    }
+}
+
+export const getAllWithdrawalRequest = jwt => async dispatch => {
+    dispatch({ type: GET_WITHDRAWAL_REQUEST_REQUEST })
+
+    try {
+        const response = await api.get(`/api/admin/withdrawal`, {
+            headers: {
+                Authorization: `Bearer ${jwt}`
+            }
+        })
+        
+        dispatch({
+            type: GET_WITHDRAWAL_REQUEST_SUCCESS,
+            payload: response.data
+       })
+    } catch (error) {
+        console.log(error)
+        dispatch({
+            type: GET_WITHDRAWAL_REQUEST_FAILURE,
+            payload: error.message
+        })
+    }
 }
