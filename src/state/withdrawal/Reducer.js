65a0fcb9f4e618011f5a6f08fcca7aeea162bc34
DilyanSvetiev <dsvetiev@gmail.com>
2024-11-22 18:23:51 +0200

    add: add withdrawalReducer for implementation

---
+++ src/state/withdrawal/Reducer.js
@@ -0,0 +1,7 @@
+import { WITHDRAWAL_REQUEST } from "./ActionTypes"
+
+const withdrawalReducer = (state = iniitalState, action) => {
+    switch(action.type) {
+        case WITHDRAWAL_REQUEST
+    }
+}
