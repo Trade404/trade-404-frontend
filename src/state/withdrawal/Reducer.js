0751ba485b058181b7271a495beb5c77228495e3
DilyanSvetiev <dsvetiev@gmail.com>
2024-11-23 22:04:16 +0200

    add: implement withdrawalReducer cases based on request

--- src/state/withdrawal/Reducer.js
+++ src/state/withdrawal/Reducer.js
@@ -1,7 +1,15 @@
-import { WITHDRAWAL_REQUEST } from "./ActionTypes"
+import { GET_WITHDRAWAL_HISTORY_REQUEST, GET_WITHDRAWAL_REQUEST_REQUEST, WITHDRAWAL_PROCEED_REQUEST, WITHDRAWAL_REQUEST } from "./ActionTypes"
 
 const withdrawalReducer = (state = iniitalState, action) => {
     switch(action.type) {
-        case WITHDRAWAL_REQUEST
+        case WITHDRAWAL_REQUEST:
+        case WITHDRAWAL_PROCEED_REQUEST:
+        case GET_WITHDRAWAL_HISTORY_REQUEST:
+        case GET_WITHDRAWAL_REQUEST_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                error: null
+            };
     }
 }
