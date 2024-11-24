40782efaf0cab60f54911bdd3bc2bc5e0f312f94
DilyanSvetiev <dsvetiev@gmail.com>
2024-11-24 23:17:46 +0200

    add: withdrawal, add withdrawal and get withdrawal sucess cases

--- src/state/withdrawal/Reducer.js
+++ src/state/withdrawal/Reducer.js
@@ -1,6 +1,6 @@
-import { GET_WITHDRAWAL_HISTORY_REQUEST, GET_WITHDRAWAL_REQUEST_REQUEST, WITHDRAWAL_PROCEED_REQUEST, WITHDRAWAL_REQUEST } from "./ActionTypes"
+import { ADD_PAYMENT_DETAILS_SUCCESS, GET_PAYMENT_DETAILS_SUCCESS, GET_WITHDRAWAL_HISTORY_REQUEST, GET_WITHDRAWAL_REQUEST_REQUEST, WITHDRAWAL_PROCEED_REQUEST, WITHDRAWAL_REQUEST, WITHDRAWAL_SUCCESS } from "./ActionTypes"
 
-const withdrawalReducer = (state = iniitalState, action) => {
+const withdrawalReducer = (state = initialState, action) => {
     switch(action.type) {
         case WITHDRAWAL_REQUEST:
         case WITHDRAWAL_PROCEED_REQUEST:
@@ -11,5 +11,20 @@ const withdrawalReducer = (state = iniitalState, action) => {
                 loading: true,
                 error: null
             };
+        case WITHDRAWAL_SUCCESS:
+            return {
+                ...state,
+                withdrawal: action.payload,
+                loading: false,
+                error: null
+            }
+        case ADD_PAYMENT_DETAILS_SUCCESS:
+        case GET_PAYMENT_DETAILS_SUCCESS:
+            return {
+                ...state,
+                paymentDetails: action.payload,
+                loading: false,
+                error: null
+            }
     }
 }
