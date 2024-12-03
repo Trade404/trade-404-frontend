4e03d93e3dffa775137cdcf574cae946724eae20
DilyanSvetiev <dsvetiev@gmail.com>
2024-12-03 17:39:59 +0200

    add: implement Withdrawal.jsx dynamic data input

--- src/page/Withdrawal/Withdrawal.jsx
+++ src/page/Withdrawal/Withdrawal.jsx
@@ -1,7 +1,20 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { store } from "@/state/Store";
+import { getWIthdrawalHistory } from "@/state/withdrawal/Action";
 import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Withdrawal = () => {
+
+  const dispatch = useDispatch()
+  const { wallet, Withdrawal } = useSelector(store => store)
+
+  useEffect(() => {
+    dispatch(getWIthdrawalHistory(localStorage.getItem('jwt')))
+  },[])
+
+
     return (
                 <div className="p-5 lg:p-20">
         <h1 className="font-bold text-3xl pb-5">Withdrawal</h1>
@@ -15,13 +28,13 @@ const Withdrawal = () => {
     </TableRow>
   </TableHeader>
   <TableBody>
-    {[1,1,1,1,1,1].map((item, index) =>     
+    {withdrawal.history.map((item, index) =>     
     <TableRow key={index}>
-      <TableCell>22/10/2024</TableCell>
+      <TableCell>{item.date.toString()}</TableCell>
       <TableCell>Bank Account</TableCell>
-      <TableCell>$68221</TableCell>
+      <TableCell>${item.amount}</TableCell>
       <TableCell className="text-right">
-      303
+      {item.status}
       </TableCell>
     </TableRow>)}
   </TableBody>
