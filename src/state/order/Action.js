e58287538a4b7bcc24e246206eb2f6a984561263
DilyanSvetiev <dsvetiev@gmail.com>
2024-12-07 23:45:00 +0200

    add: implement getOrderById function

--- src/state/order/Action.js
+++ src/state/order/Action.js
@@ -1,5 +1,5 @@
 import { api } from "@/config/api";
-import { PAY_ORDER_FAILURE, PAY_ORDER_REQUEST, PAY_ORDER_SUCCESS } from "./ActionTypes"
+import { GET_ORDER_FAILURE, GET_ORDER_REQUEST, GET_ORDER_SUCCESS, PAY_ORDER_FAILURE, PAY_ORDER_REQUEST, PAY_ORDER_SUCCESS } from "./ActionTypes"
 
 
 export const payOrder = ({jwt, orderData, amount}) => async (dispatch) => {
@@ -20,6 +20,28 @@ export const payOrder = ({jwt, orderData, amount}) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: PAY_ORDER_FAILURE,
+            error: error.message
+        })
+    }
+}
+
+export const getOrderById = ({jwt, orderId}) => async (dispatch) => {
+    dispatch({type: GET_ORDER_REQUEST});
+
+    try {
+        const response = await api.get(`/api/orders/${orderId}`, {
+            headers: {
+                Authorization: `Bearer ${jwt}`
+            }
+        })
+
+        dispatch({
+            type: GET_ORDER_SUCCESS,
+            payload: response.data,
+        })
+    } catch (error) {
+        dispatch({
+            type: GET_ORDER_FAILURE,
             error: error.message
         })
     }
