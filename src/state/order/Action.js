e483df33b41d1906191c85990f431023268b3d4a
DilyanSvetiev <dsvetiev@gmail.com>
2024-12-08 19:22:19 +0200

    add: implement getAllOrdersForUser function

--- src/state/order/Action.js
+++ src/state/order/Action.js
@@ -1,5 +1,5 @@
 import { api } from "@/config/api";
-import { GET_ORDER_FAILURE, GET_ORDER_REQUEST, GET_ORDER_SUCCESS, PAY_ORDER_FAILURE, PAY_ORDER_REQUEST, PAY_ORDER_SUCCESS } from "./ActionTypes"
+import { GET_ALL_ORDER_FAILURE, GET_ALL_ORDER_REQUEST, GET_ALL_ORDER_SUCCESS, GET_ORDER_FAILURE, GET_ORDER_REQUEST, GET_ORDER_SUCCESS, PAY_ORDER_FAILURE, PAY_ORDER_REQUEST, PAY_ORDER_SUCCESS } from "./ActionTypes"
 
 
 export const payOrder = ({jwt, orderData, amount}) => async (dispatch) => {
@@ -42,6 +42,32 @@ export const getOrderById = ({jwt, orderId}) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: GET_ORDER_FAILURE,
+            error: error.message
+        })
+    }
+}
+
+export const getAllOrdersForUser = ({jwt, orderType, assetSymbol}) => async (dispatch) => {
+    dispatch({type: GET_ALL_ORDER_REQUEST});
+
+    try {
+        const response = await api.get(`/api/orders`, {
+            headers: {
+                Authorization: `Bearer ${jwt}`
+            },
+            params: {
+                order_type: orderType,
+                asset_symbol: assetSymbol
+            }
+        })
+
+        dispatch({
+            type: GET_ALL_ORDER_SUCCESS,
+            payload: response.data
+        })
+    } catch (error) {
+        dispatch({
+            type: GET_ALL_ORDER_FAILURE,
             error: error.message
         })
     }
